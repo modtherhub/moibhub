83ed890f500bd4d85f78c0580c4f4b939e1d15e3
modtherhub <moibhub@gmail.com>
2025-05-26 09:09:55 +0400

    Edit button

--- src/App.jsx
+++ src/App.jsx
@@ -48,13 +48,14 @@ function App() {
             {links.map(({text,link,color}, index) => {
 
               return(
-                <a href={link} key={index} target="_blank">
-						      <div
-							    className={`w-80 sm:w-96 mx-auto ${color} text-center text-xl font-bold py-3 border-2 border-black shadow-custom hover:shadow-none transition-all hover:translate-x-1 hover:translate-y-1`}
-						     >
-							    {text}
-						      </div>
-					      </a>
+                <a href={link} target="_blank" className="w-80 sm:w-96 mx-auto relative inline-block text-lg group">
+                  <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                      <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                      <span className="absolute left-0 w-98 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                      <span className="relative items-center justify-center flex">{text}</span>
+                  </span>
+                  <span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+                </a>
               )
 
             })}
